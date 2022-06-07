645dedb71c3ba89e4254332f30728ca05833480e
Eyüphan Akkaya <96181594+eyuphanakkaya@users.noreply.github.com>
2022-06-07 21:59:36 +0300

    Admin Duzenleme Paneli
    Bu Uygulama;
    Yapmış olduğum uygulama admin panelidir ve sadece admin tarafından kullanılabilir. Kullanıcının kullanabilmesi için geliştirilebilir.
     Admin tarafından sitemizde olan;
    •	Müşterileri,
    •	Kategorileri,
    •	Ürünleri,
    •	Satışları ,
    •	İstatistikleri
    kolaylıkla görüp bunların ekleme silme güncelleme işlemlerini gerçekleştirebilmek için hazırlanmıştır.
    Veri tabanı sunucusu olarak MSSQL Server kullanılmıştır.
    Projemin SQL bağlantısını Entity Framework üzerinden Modül oluşturarak gerçekleştirdim. Web sitemi ise Visual Studio üzerinden Asp.net kullanarak gerçekleştirdim

---
+++ veritabanıproje2/odev3/odev3/KategoriGuncelle.aspx.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using odev3.Entity;
+
+namespace odev3
+{
+    public partial class KategoriGuncelle : System.Web.UI.Page
+    {
+        odev3Entities3 db = new odev3Entities3();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (Page.IsPostBack==false)
+            {
+                int id = Convert.ToInt32(Request.QueryString["kategoriid"]);
+                Txtid.Text = id.ToString();
+                var ktgr = db.tblkategori.Find(id);
+                Txtad.Text = ktgr.kategoriad;
+            }
+           
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            
+            int id = Convert.ToInt32(Request.QueryString["kategoriid"]);
+            var ktgr = db.tblkategori.Find(id);
+            ktgr.kategoriad = Txtad.Text;
+            db.SaveChanges();
+            Response.Redirect("Kategoriler.Aspx");
+        }
+    }
+}
---
+++ veritabanıproje2/odev3/odev3/Kategoriler.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using odev3.Entity;
+
+namespace odev3
+{
+    public partial class Kategoriler : System.Web.UI.Page
+    {
+        odev3Entities3 db = new odev3Entities3();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            var degerler = db.tblkategori.ToList();
+            Repeater1.DataSource = degerler;
+            Repeater1.DataBind();
+        }
+    }
+}
